e6e86430b7e45a8edf01490c6eeccb20e7c200db
Daniel Fitzick <incherre2526@gmail.com>
2018-01-01 19:21:13 -0500

    Added the skeleton of unit tests. Updated filenames to adhere with python convention.

---
+++ code/books_common_unittests.py
@@ -0,0 +1,133 @@
+'''This is the unit test file for the books-common.py file.'''
+
+import unittest
+import books_common
+
+class TestBookMethods(unittest.TestCase):
+    def test_basic_init(self):
+        pass #TODO
+
+    def test_full_init(self):
+        pass #TODO
+
+    def test_delete(self):
+        pass #TODO
+
+    def test_getTitle(self):
+        pass #TODO
+
+    def test_getAuthorName(self):
+        pass #TODO
+
+    def test_compare(self):
+        pass #TODO
+
+class TestUserMethods(unittest.TestCase):
+    def test_basic_init(self):
+        pass #TODO
+
+    def test_full_init_(self):
+        pass #TODO
+
+    def test_getBooks(self):
+        pass #TODO
+
+    def test_getUserName(self):
+        pass #TODO
+
+    def test_getNumBooks(self):
+        pass #TODO
+
+    def test_getFormLink(self):
+        pass #TODO
+
+class TestDateMethods(unittest.TestCase):
+    def test_basic_init(self):
+        pass #TODO
+
+    def test_full_init(self):
+        pass #TODO
+
+    def test_getStringDate(self):
+        pass #TODO
+
+    def test_getYear(self):
+        pass #TODO
+
+    def test_getMonth(self):
+        pass #TODO
+
+    def test_getDay(self):
+        pass #TODO
+
+    def test_compare(self):
+        pass #TODO
+
+class TestPollMethods(unittest.TestCase):
+    def test_basic_init(self):
+        pass #TODO
+
+    def test_full_init(self):
+        pass #TODO
+
+    def test_getWinner(self):
+        pass #TODO
+
+    def test_closeVoting(self):
+        pass #TODO
+
+    def test_getFormLink(self):
+        pass #TODO
+
+    def test_getOptions(self):
+        pass #TODO
+
+    def test_getDate(self):
+        pass #TODO
+
+    def test_updateResults(self):
+        pass #TODO
+
+class TestLocationMethods(unittest.TestCase):
+    def test_basic_init(self):
+        pass #TODO
+
+    def test_compare(self):
+        pass #TODO
+
+class TestDataIOMethods(unittest.TestCase):
+    def test_basic_init(self):
+        pass #TODO
+
+    def test_getUserNames(self):
+        pass #TODO
+
+    def test_getUserInfo(self):
+        pass #TODO
+
+    def test_getUserBooks(self):
+        pass #TODO
+
+    def test_getHistory(self):
+        pass #TODO
+
+    def test_getCurrentPoll(self):
+        pass #TODO
+
+    def test_createUser(self):
+        pass #TODO
+
+    def test_removeBook(self):
+        pass #TODO
+
+    def test_newPoll(self):
+        pass #TODO
+
+    def test_closePoll(self):
+        pass #TODO
+
+    def test_addWinner(self):
+        pass #TODO
+
+if __name__ == '__main__':
+    unittest.main()
