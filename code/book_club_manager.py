d34abc3d6313e31becadd6e5f2e890ac3a816782
Daniel Fitzick <incherre2526@gmail.com>
2018-02-28 11:40:13 -0500

    Implemented viewing poll info, viewing history, and deleting users via the UI. Added some error handling where it had been overlooked.

--- code/book_club_manager.py
+++ code/book_club_manager.py
@@ -126,8 +126,22 @@ if __name__ == '__main__':
     #----- External Functionality -----
     def view_poll_info():
         '''Displays the info for the current poll.'''
-        print('poll info')
-        #TODO(incherre): Add functionality
+        possible_errors = (google_api.errors.HttpError,
+                           google_api.AppsScriptError,
+                           google_api.SpreadsheetFormatError)
+        try:
+            current_poll = BOOK_BOT.get_current_poll()
+        except possible_errors:
+            print('Failed to retrieve current poll')
+        else:
+            if current_poll is None:
+                print('There is no currently active poll')
+            else:
+                print('Current Poll:')
+                print(' Date created: %s' % (current_poll.get_date().get_string_date()))
+                print(' Options:')
+                for book in current_poll.get_options():
+                    print('  "%s" by %s' % (book.get_title(), book.get_author_name()))
 
     def start_new_poll():
         '''Deletes the old poll and begins a new poll.'''
@@ -146,13 +160,30 @@ if __name__ == '__main__':
 
     def view_history():
         '''Displays the history.'''
-        print('history')
-        #TODO(incherre): Add functionality
+        possible_errors = (google_api.errors.HttpError,
+                           google_api.AppsScriptError,
+                           google_api.SpreadsheetFormatError)
+        try:
+            history = BOOK_BOT.get_history()
+        except possible_errors:
+            print('Failed to retrieve history')
+            histroy = []
+
+        for record in history:
+            print('%s: "%s" by %s %s' % tuple(record))
 
     def delete_user(user):
         '''Removes a user from the records.'''
-        print(user.get_user_name() + ' removed')
-        #TODO(incherre): Add functionality
+        possible_errors = (google_api.errors.HttpError,
+                           google_api.AppsScriptError,
+                           google_api.SpreadsheetFormatError)
+
+        try:
+            BOOK_BOT.remove_user(user)
+        except possible_errors:
+            print('Failed to remove user')
+        else:
+            print(user.get_user_name() + ' removed')
 
     def remove_all_books(user):
         '''Removes all the books of a user.'''
@@ -161,15 +192,30 @@ if __name__ == '__main__':
 
     def remove_book(book):
         '''Removes the book from the records.'''
-        book.delete()
-        print(book.get_title() + ' removed')
+        possible_errors = (TypeError,
+                           google_api.errors.HttpError,
+                           google_api.AppsScriptError)
+        try:
+            book.delete()
+        except possible_errors:
+            print('Book deletion failed')
+        else:
+            print(book.get_title() + ' removed')
     #----- End External Functionality -----
 
     #----- Define Menu Structure -----
     def book_gen(go_up, user):
         '''Generates a list of book menu objects.'''
+        possible_errors = (google_api.errors.HttpError,
+                           google_api.AppsScriptError,
+                           google_api.SpreadsheetFormatError)
+        try:
+            books = BOOK_BOT.get_user_books(user)
+        except possible_errors:
+            print('Failed to retrieve books')
+            books = []
+
         book_delete_options = []
-        books = BOOK_BOT.get_user_books(user)
 
         for book in books:
             temp_option = MenuItem('Delete "%s" by %s' % (book.get_title(), book.get_author_name()),
@@ -180,27 +226,41 @@ if __name__ == '__main__':
 
     def user_gen(go_up):
         '''Generates a list of user menu objects.'''
-        user_names = BOOK_BOT.get_user_names()
+        possible_errors = (google_api.errors.HttpError,
+                           google_api.AppsScriptError,
+                           google_api.SpreadsheetFormatError)
+        try:
+            user_names = BOOK_BOT.get_user_names()
+        except possible_errors:
+            print('Failed to retrieve user list')
+            user_names = []
+
         user_menus = []
 
         for name in user_names:
+            valid_user = True
             if not name in USERS:
-                USERS[name] = BOOK_BOT.get_user_info(name)
+                try:
+                    USERS[name] = BOOK_BOT.get_user_info(name)
+                except possible_errors:
+                    print("Failed to retrieve %s's info" % (name))
+                    valid_user = False
 
-            gen_book_function = make_lambda(book_gen, go_up, USERS[name])
-            temp_remove_book = DynamicMenu('Remove one of ' + name + "'s books",
-                                           gen_book_function, [go_up])
+            if valid_user:
+                gen_book_function = make_lambda(book_gen, go_up, USERS[name])
+                temp_remove_book = DynamicMenu("Remove one of %s's books" % (name),
+                                               gen_book_function, [go_up])
 
-            remove_books_function = make_lambda(remove_all_books, USERS[name])
-            temp_remove_books = MenuItem('Remove all of ' + name + "'s books",
-                                         remove_books_function)
+                remove_books_function = make_lambda(remove_all_books, USERS[name])
+                temp_remove_books = MenuItem("Remove all of %s's books" % (name),
+                                             remove_books_function)
 
-            del_user_function = make_lambda(delete_user, USERS[name])
-            temp_del_user = MenuItem('Delete ' + name, del_user_function)
+                del_user_function = make_lambda(delete_user, USERS[name])
+                temp_del_user = MenuItem('Delete %s' % (name), del_user_function)
 
-            temp_menu = HighLevelMenu(name, [temp_remove_book, temp_remove_books,
-                                             temp_del_user, go_up])
-            user_menus.append(temp_menu)
+                temp_menu = HighLevelMenu(name, [temp_remove_book, temp_remove_books,
+                                                 temp_del_user, go_up])
+                user_menus.append(temp_menu)
 
         return user_menus
 
